d40f33564712726d5fd2d0d2ca9a10bb7d3b39e9
JeetShah <jeetsh4h@gmail.com>
2024-12-03 11:57:17 +0530

    day 3 answers

---
+++ Day03/part_one.py
@@ -0,0 +1,11 @@
+import re
+from functools import reduce
+from operator import mul, add
+
+corrupted_instruction = reduce(add, open('./Day03/input.txt').readlines())
+real_instructions = re.findall(r'mul\(\d{1,3},\d{1,3}\)', corrupted_instruction)
+
+print(sum(reduce(mul, map(int, instr[4:-1].split(','))) for instr in real_instructions))
+
+# test_input => 161
+# input => 168539636
