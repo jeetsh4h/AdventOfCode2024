dda2c76078bd6c9125d67745a69cb44c74ea6c92
JeetShah <jeetsh4h@gmail.com>
2024-12-03 10:38:51 +0530

    day 1 answers

---
+++ Day01/part_one.py
@@ -0,0 +1,13 @@
+with open('./Day01/input.txt') as f:
+    lines = f.readlines()
+
+# could probably insert the elements sorted here, for optimisation
+list1, list2 = [], []
+for line in lines:
+    elements = line.split()
+    list1.append(int(elements[0]))
+    list2.append(int(elements[1]))
+
+print(sum(abs(x - y) for x, y in zip(sorted(list1), sorted(list2))))
+# test_input => 11
+# input => 2769675
---
+++ Day01/part_two.py
@@ -0,0 +1,21 @@
+from collections import defaultdict
+
+with open('./Day01/input.txt') as f:
+    lines = f.readlines()
+
+# element1: frequency_in_list1
+frequency1 = defaultdict(lambda : 0)
+frequency2 = defaultdict(lambda : 0)
+
+for line in lines:
+    elements = line.split()
+    frequency1[int(elements[0])] += 1
+    frequency2[int(elements[1])] += 1
+
+answer = 0
+for k, v in frequency1.items():
+    answer += (k * frequency2[k]) * v
+
+print(answer)
+# test_input => 31
+# input => 24643097
